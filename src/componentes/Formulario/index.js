efca401efde038c5bb3f37005b855907e305de1e
Soilenyis <soilenyis@gmail.com>
2024-03-29 14:17:56 -0300

    criando botão

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,40 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
+    return (
+        <section className='formulario'> 
+            <form onSubmit={aoSalvar}> 
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+
+}
+
+export default Formulario
