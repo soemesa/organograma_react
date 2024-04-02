9cd481cec50784d825eb424de144cb70381be4f5
Soilenyis <soilenyis@gmail.com>
2024-04-02 08:41:20 -0300

    interagindo com o usuario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,22 @@
 import './CampoTexto.css'
+import { useState } from 'react'
 
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`
+
+    // let valor = ''
+
+    const [valor, setValor] = useState('')
+    
+    const aoDigitado =  (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
